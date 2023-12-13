10febc8d58579fd1306e947a116748927ae7936a
LuisferRojas14 <LuisferRojas14>
2023-12-13 16:25:18 -0500

    modifiqué estilos

--- lib/config/theme/theme_app.dart
+++ lib/config/theme/theme_app.dart
@@ -28,5 +28,8 @@ class AppTheme {
   ThemeData getTheme() => ThemeData(
         useMaterial3: true,
         colorSchemeSeed: colorList[selectedColor],
+        appBarTheme: const AppBarTheme(
+          centerTitle: false,
+        )
       );
 }
