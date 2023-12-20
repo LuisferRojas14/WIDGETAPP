5c24cf335960a32c875505112755c8ea94898abf
LuisferRojas14 <luisfernandorojas@unicesar.edu.co>
2023-12-20 18:59:36 -0500

    buttons screens final

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -63,7 +63,7 @@ class _ButtonsView extends StatelessWidget {
               label: const Text('Text Icon'),
             ),
 
-            //Todo custom button
+           const CustomButton(),
 
             IconButton(onPressed: (){}, icon: const Icon(Icons.app_registration_rounded)),
             IconButton(onPressed: (){}, icon: const Icon(Icons.app_registration_rounded),
@@ -75,6 +75,29 @@ class _ButtonsView extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: (){},
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text('Hola Mundo',
+            style: TextStyle(color: Colors.white),
+            ),
+          ),
+        )),
     );
   }
 }
