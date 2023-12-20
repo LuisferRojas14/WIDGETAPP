cca3e879e2233cc79af6995b761158f97f577af3
LuisferRojas14 <luisfernandorojas@unicesar.edu.co>
2023-12-20 16:45:12 -0500

    add_buttons

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ButtonScreen extends StatelessWidget {
   static const String name = 'button_screen';
@@ -9,8 +10,13 @@ class ButtonScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Botones'),
-    ),
-    body: const _ButtonsView(),
+      ),
+      body: const _ButtonsView(),
+      floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.arrow_back_ios_new_rounded),
+          onPressed: () {
+            context.pop();
+          }),
     );
   }
 }
@@ -20,6 +26,23 @@ class _ButtonsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SizedBox(
+      width:double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        child: Wrap(
+          spacing: 10,
+          children: [
+            ElevatedButton(onPressed: () {}, child: const Text('Elevated')),
+            const ElevatedButton(
+                onPressed:null, child: Text('Elevated Disabled')),
+            ElevatedButton.icon(onPressed: (){},
+            icon: const Icon (Icons.access_alarm_rounded),
+            label: const Text('Elevated Icon'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
